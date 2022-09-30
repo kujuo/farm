54f2f72426861c99ea4d7cb9a08d42cb580db8ab
Veerin Bussabun <veerinbussabun@Veerins-MacBook-Air.local>
2022-09-30 17:58:09 +0200

    ui

--- Assets/Scripts/NewUIInventory.cs
+++ Assets/Scripts/NewUIInventory.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class NewUIInventory : MonoBehaviour
-{
-    //attach this script to UI_Inventory gameobject
-    public Text TextNumCrop;
-    public Text TextNumBuilding;
+//public class NewUIInventory : MonoBehaviour
+//{
+//    //attach this script to UI_Inventory gameobject
+//    public Text TextNumCrop;
+//    public Text TextNumBuilding;
 
-    private void Start()
-    {
-        TextNumCrop = transform.Find("TextNumCrop");
-        TextNumBuilding = transform.Find("TextNumBuilding");
+//    private void Start()
+//    {
+//        TextNumCrop = transform.Find("TextNumCrop");
+//        TextNumBuilding = transform.Find("TextNumBuilding");
 
-    }
+//    }
     
-}
+//}
