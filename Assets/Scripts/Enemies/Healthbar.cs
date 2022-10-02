a820e280ea32d3a17f4236efdab2b15e56db0bf5
Julia Hou <juahou@gmail.com>
2022-10-02 17:23:10 +0200

    healthbar for player

--- Assets/Scripts/Enemies/Healthbar.cs
+++ Assets/Scripts/Enemies/Healthbar.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
+    public bool activated = false;
     private Image HealthBarImage;
     // Start is called before the first frame update
     void Start()
     {
         HealthBarImage = GetComponent<Image>();
         SetHealthBarColor(new Color(1, 1, 1, 0));
+        if (activated) SetHealthBarColor(new Color(0, 255, 0, 1));
     }
 
     public void Activate()
@@ -43,5 +45,12 @@ public class Healthbar : MonoBehaviour
     public void SetHealthBarColor(Color healthColor)
     {
         HealthBarImage.color = healthColor;
+    }
+
+    public void SetHealthBarAlpha(float a)
+    {
+        Color col = HealthBarImage.color;
+        col.a = a;
+        HealthBarImage.color = col;
     }
 }
---
+++ Assets/Scripts/Player/PlayerStatusManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatusManager : MonoBehaviour
+{
+    public Healthbar playerHealth;
+
+    public void updateHealth(float health, float maxHealth)
+    {
+        playerHealth.SetHealthBarValue(health / maxHealth);
+        playerHealth.SetHealthBarAlpha(0.6f);
+    }
+}
